d81140048737f417193cf306bdc71addbf7c3a51
joojonghyeon <jjh930301@gmail.com>
2023-08-13 17:51:46 +0900

    [Added] webflux 패키지 , DB constants

--- admin/src/main/java/com/spring/container/spring/constants/Constant.java
+++ admin/src/main/java/com/spring/container/spring/constants/Constant.java
@@ -2,4 +2,9 @@ package com.spring.container.spring.constants;
 
 public class Constant {
   public static final String COLLATE = " CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ";
+  public static final String DB_URI = System.getenv("DB_URI");
+  public static final String ADMIN_DB = System.getenv("ADMIN_DB");
+  public static final String BOOSTER_DB = System.getenv("BOOSTER_DB");
+  public static final String DB_USER = System.getenv("DB_USER");
+  public static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
 }
