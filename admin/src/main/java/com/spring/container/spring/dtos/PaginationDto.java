97fc4bd7a2180bce7d1ac876fe2355d9fafd2b3e
joojonghyeon <jjh930301@gmail.com>
2023-08-16 12:15:24 +0900

    [Changed] DateTimeFormat requiredMode

--- admin/src/main/java/com/spring/container/spring/dtos/PaginationDto.java
+++ admin/src/main/java/com/spring/container/spring/dtos/PaginationDto.java
@@ -1,8 +1,11 @@
 package com.spring.container.spring.dtos;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,23 +13,29 @@ import lombok.Setter;
 @Setter
 public class PaginationDto {
   @Schema(
-    name = "page",
-    required = true
+    required = true,
+    requiredMode = RequiredMode.REQUIRED
   )
   private int page;
   @Schema(
-    name = "count",
-    required = true
+    required = true,
+    requiredMode = RequiredMode.REQUIRED
   )
   private int count;
 
   @Schema(
-    required = false
-  )
-  private LocalDate start_date;
+    required = false,
+    example = "2023-01-18T00:00:00",
+    requiredMode = RequiredMode.NOT_REQUIRED
+)
+  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  private LocalDateTime start_date;
   
   @Schema(
-    required = false
+    required = false,
+    example = "2023-02-18T00:00:00",
+    requiredMode = RequiredMode.NOT_REQUIRED
   )
-  private LocalDate end_date;
+  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  private LocalDateTime end_date;
 }
--- admin/src/main/java/com/spring/container/spring/dtos/UsersDto.java
+++ admin/src/main/java/com/spring/container/spring/dtos/UsersDto.java
@@ -1,45 +1,52 @@
 package com.spring.container.spring.dtos;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UsersDto extends PaginationDto {
+  
   @Schema(
-    description = "0 : 사용 <br>" +
-    "1 : 휴면 <br>" +
-    "2 : 중지 <br>" +
-    "3 : 탈퇴 ",
-    required = false
+    description = "1 : 사용 <br>" +
+    "2 : 휴면 <br>" +
+    "3 : 중지 <br>" +
+    "4 : 탈퇴 ",
+    required = false,
+    requiredMode = RequiredMode.NOT_REQUIRED
   )
   private int type;
   @Schema(
-    description = "0 : 남자 <br>" +
-    "1 : 여자",
-    required = false
+    description = "1 : 남자 <br>" +
+    "2 : 여자",
+    required = false,
+    requiredMode = RequiredMode.NOT_REQUIRED
   )
   private int gender;
   @Schema(
     description = "1 : 사업자가 등록된 유저 <br>" +
     "2 : 사업자가 등록되지 않은 유저",
-    required = false
+    required = false,
+    requiredMode = RequiredMode.NOT_REQUIRED
   )
   private int business;
   @Schema(
     description = "1 : 연결기관 연결된 유저 <br>" +
     "2 : 연결기관이 연결되지 않은 유저",
-    required = false
+    required = false,
+    requiredMode = RequiredMode.NOT_REQUIRED
   )
   private int connected;
   @Schema(
-    description = "0 : 아무것도 신청하지 않은 유저 <br>" + 
-    "1 : 선정산 유저 <br>" +
-    "2 : 선정산 신청중인 유저 <br>" +
-    "3 : 선정산 반려된 유저 <br>" +
-    "4 : 선정산 해지된 유저",
-    required = false
+    description = "1 : 아무것도 신청하지 않은 유저 <br>" + 
+    "2 : 선정산 유저 <br>" +
+    "3 : 선정산 신청중인 유저 <br>" +
+    "4 : 선정산 반려된 유저 <br>" +
+    "5 : 선정산 해지된 유저",
+    required = false,
+    requiredMode = RequiredMode.NOT_REQUIRED
   )
   private int is_paid;
   @Schema(
@@ -50,8 +57,10 @@ public class UsersDto extends PaginationDto {
     description = "1 : 회원번호 <br>" +
     "2 : 이름 <br>" +
     "3 : 휴대폰 번호 <br>" +
-    "4 : 사업자 번호" 
+    "4 : 사업자 번호" ,
+    required = false,
+    requiredMode = RequiredMode.NOT_REQUIRED
   )
-  private String search;
+  private int search;
 
 }
